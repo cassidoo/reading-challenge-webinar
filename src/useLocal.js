91a5217540d6b4c0d7cf051c36193e6e1a88d0a4
Cassidy Williams <cassidoo@users.noreply.github.com>
2020-04-16 15:00:12 -0700

    Fix initial local storage bug

--- src/useLocal.js
+++ src/useLocal.js
@@ -6,10 +6,12 @@ export default function useLocal() {
   let [state, dispatch] = useAppState();
 
   useEffect(() => {
+    let toReadLocal = localStorage.getItem("to_read_list");
+    let completedLocal = localStorage.getItem("completed_list");
     dispatch({
       type: "GET_FROM_LOCAL_STATE",
-      toRead: localStorage.getItem("to_read_list").split(",") || BOOKS,
-      completed: localStorage.getItem("completed_list").split(",") || [],
+      toRead: toReadLocal ? toReadLocal.split(",") : BOOKS,
+      completed: completedLocal ? completedLocal.split(",") : [],
     });
   }, [dispatch]);
 }
