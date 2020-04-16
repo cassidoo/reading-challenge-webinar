e61d6a4b85c27874a1853312e48d47b1df779228
Mateo Durante <mateo.durante@globant.com>
2020-04-16 18:45:11 -0300

    fix initial localstorage

--- src/useLocal.js
+++ src/useLocal.js
@@ -8,8 +8,8 @@ export default function useLocal() {
   useEffect(() => {
     dispatch({
       type: "GET_FROM_LOCAL_STATE",
-      toRead: localStorage.getItem("to_read_list").split(",") || BOOKS,
-      completed: localStorage.getItem("completed_list").split(",") || [],
+      toRead: localStorage.getItem("to_read_list") ? localStorage.getItem("to_read_list").split(",") : BOOKS,
+      completed: localStorage.getItem("completed_list") ? localStorage.getItem("completed_list").split(",") : [],
     });
   }, [dispatch]);
 }
