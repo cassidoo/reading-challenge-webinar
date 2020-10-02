bb40699e02a300e3494a481147c49a89d1af8965
tiana30 <tiana3059@gmail.com>
2020-10-03 06:04:54 +0700

    Revert "Beginning state of webinar"
    This reverts commit 5a93d94667bd32be2759643450c8cc6a8d786397.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { AppStateProvider } from "./AppContext";
 import BookList from "./components/BookList";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Reading Challenge</header>
-      <div className="lists">
-        <BookList />
-        <BookList completeList />
+    <AppStateProvider>
+      <div className="App">
+        <header className="App-header">Reading Challenge</header>
+        <div className="lists">
+          <BookList />
+          <BookList completeList />
+        </div>
       </div>
-    </div>
+    </AppStateProvider>
   );
 }
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { BOOKS } from "../constants";
+import { useAppState } from "../AppContext";
 
 export default function BookList({ completeList }) {
+  let [state, dispatch] = useAppState();
+
+  let { toRead, completed } = state;
+
   return (
     <div>
       <h1>{completeList ? `Completed` : `To Read`}</h1>
       <ul>
         {!completeList &&
-          BOOKS.map((book, index) => {
+          toRead.map((book, index) => {
             return (
               <li
                 key={index}
                 onClick={() => {
-                  console.log("do something");
+                  dispatch({ type: "READ_BOOK", item: book });
+                }}
+              >
+                {book}
+              </li>
+            );
+          })}
+
+        {completeList &&
+          completed.map((book, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => {
+                  dispatch({ type: "UNREAD_BOOK", item: book });
                 }}
               >
                 {book}
